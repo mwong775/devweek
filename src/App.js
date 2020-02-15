fbf3d0666f2ca63271e830382116ed6bc5bce7eb
mwong775 <mwong775@gmail.com>
2020-02-15 15:01:14 -0800

    add signin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './SignIn'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
+          <SignIn/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
