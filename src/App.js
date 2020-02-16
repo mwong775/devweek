a9468122655ab3c587391e20aaa88c5496bd5d57
Paul Yakovlev <yvpavel@gmail.com>
2020-02-15 16:50:38 -0800

    add splash page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./App.css";
 import SignInPage from "./SignInPage";
 import FlightsPage from "./FlightsPage";
+import SplashPage from "./SplashPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={SignInPage} />
-        <Route path="/flights" component={FlightsPage} />
+        <Switch>
+          <Route path="/" exact component={SplashPage} />
+          <Route path="/signIn" component={SignInPage} />
+          <Route path="/flights" component={FlightsPage} />
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/SplashPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function SplashPage() {
+  return (
+    <div>
+      <h1>Splash Page</h1>
+    </div>
+  );
+}
+
+export default SplashPage;
